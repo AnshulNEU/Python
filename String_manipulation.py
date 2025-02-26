c27230a14d693015c74cba863d4ef3fc231512ac
AnshulNEU <singh.anshul@northeastern.edu>
2025-02-25 20:56:33 -0500

    String_manipulation String_manipulation

--- String_manipulation.py
+++ String_manipulation.py
@@ -1,2 +1,20 @@
 str1 = "annotate"
 print(str1[3])
+
+word = "learning"
+print(word[1:5])
+
+word = "embossed"
+print(word[2:6])
+
+word = "climb"
+print(word[1:5])
+
+word = "steamer"
+start = 1
+end = 5
+print(word[start:end])
+
+str1 = "what lovely weather we're having today!"
+str2 = str1[5:9]
+print(str2)
